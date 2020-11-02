a757b417050fe79d16a5459f900124a9a4566338
DAVIDE <davideamabile81gmail.com>
2020-11-02 15:28:33 +0100

    migrate

---
+++ database/migrations/2020_11_02_141323_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 30);
+            $table->string("original_title", 50);
+            $table->sting("author", 50);
+            $table->text('description');
+            $table->sting("nation", 30);
+            $table->float('price', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
